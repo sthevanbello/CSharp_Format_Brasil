a30e119c133ec11bdaa3d9b60481826727ab65d5
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-08-15 14:20:54 -0300

    Convert number to extensive mode and money

---
+++ NumerosPorExtenso/Program.cs
@@ -0,0 +1,47 @@
+﻿using Caelum.Stella.CSharp.Inwords;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPorExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 75;
+            double valor2 = 25789654123;
+            double valor3 = 2885789654123;
+            double valor4 = 1;
+            double valor5 = 25.65;
+
+            string extenso = new Numero(valor).Extenso();
+            Console.WriteLine(extenso);
+
+
+            NumeroExtenso(valor2);
+            NumeroExtenso(valor3);
+            NumeroExtensoMoeda(valor4);
+            NumeroExtensoMoeda(valor5);
+
+            Console.ReadKey();
+        }
+
+        private static void NumeroExtenso(double valor)
+        {
+            string extenso = new Numero(valor).Extenso();
+
+            Console.WriteLine("\n===================\n");
+            Console.WriteLine(extenso);
+        }
+
+        private static void NumeroExtensoMoeda(double valor)
+        {
+            string extenso = new MoedaBRL(valor).Extenso();
+            Console.WriteLine("\n===================\n");
+            Console.WriteLine(extenso);
+        }
+    }
+}
---
+++ TrabalhandoComDatas/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComDatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime data = new DateTime(2020, 8, 14);
+
+
+            Debug.WriteLine(data.ToString("d", new CultureInfo("pt-BR")));
+            Debug.WriteLine(data.ToString("d"));
+
+            Console.WriteLine(data.ToString("d", new CultureInfo("pt-BR")));
+            Console.WriteLine(data.ToString("d"));
+            Console.WriteLine(data.ToString("dd/MM/yyyy"));
+            Console.WriteLine(data.ToString("MM/dd/yyyy"));
+
+            Debug.WriteLine(data.ToString("dd/MM/yyyy"));
+            Debug.WriteLine(data.ToString("MM/dd/yyyy"));
+
+            data = new DateTime(2020, 8, 14, 20, 29, 30, 987);
+            DateTime dataNow = DateTime.Now;
+            Console.WriteLine(data);
+            Console.WriteLine(data.ToString("HH:mm"));
+            Console.WriteLine(data.ToString("HH:mm:ss.fff"));
+            Console.WriteLine(data.ToString("D"));
+            Console.WriteLine(data.ToString("m"));
+            Console.WriteLine(data.ToString("M"));
+            Console.WriteLine("==============================================");
+            Console.WriteLine(dataNow.ToString("D"));
+            Console.WriteLine(dataNow.ToString("m"));
+            Console.WriteLine(dataNow.ToString("M"));
+            Console.WriteLine(dataNow.ToString("Y"));
+            Console.WriteLine(dataNow.ToString("HH:mm:ss.fff"));
+            Console.WriteLine(dataNow.ToString("G"));
+            Console.WriteLine(dataNow.ToString("g"));
+
+            Console.WriteLine(dataNow.ToString("O"));
+            Console.WriteLine(DateTime.Parse(dataNow.ToString("O")).ToString("D"));
+
+            Console.WriteLine(dataNow.ToString("t"));
+            Console.WriteLine(dataNow.ToString("T"));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
